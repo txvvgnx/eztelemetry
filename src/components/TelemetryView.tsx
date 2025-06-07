427e225ca07459ef2b1bf2da5d9ca8973e85257d
txvvgnx <txvvgnx@gmail.com>
2025-06-06 21:43:26 -0500

    remove top bar

--- src/components/TelemetryView.tsx
+++ src/components/TelemetryView.tsx
@@ -54,11 +54,11 @@ function TelemetryView() {
   }, []);
 
   return (
-    <div className="relative overflow-hidden pb-0 pt-[--topBarHeight]">
-      <TopBar />
+    <div className="relative overflow-hidden pb-0 pt-0">
+      {/* <TopBar /> */}
 
       <div className="flex w-full overflow-hidden">
-        <main className="relative flex h-[calc(100vh-var(--topBarHeight))] max-h-full w-full">
+        <main className="relative flex h-screen w-full">
           {/* Left half is live video or graphs/misc telemetry */}
           <div className="h-full w-1/2">
             <button
